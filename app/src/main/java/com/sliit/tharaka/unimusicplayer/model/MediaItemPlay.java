6c7a506884dc16f56c76eab67f33e0583df9c50f
naaaaaa <tharakamadushanki@gmail.com>
2018-03-31 13:26:45 +0530

    added ui full song play

---
+++ app/src/main/java/com/sliit/tharaka/unimusicplayer/model/MediaItemPlay.java
@@ -0,0 +1,12 @@
+package com.sliit.tharaka.unimusicplayer.model;
+
+import android.app.Activity;
+
+/**
+ * Created by ASUS on 3/31/2018.
+ */
+
+public class MediaItemPlay extends Activity {
+
+
+}
